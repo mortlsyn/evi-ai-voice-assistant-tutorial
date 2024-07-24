34930769335ea3807342b7e8c235370d4f4241ff
rachelgiahong <rachelgiahong@example.com>
2024-07-23 23:23:51 -0400

    trying to deploy newest

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,13 @@
-import { handlers } from "../../../../auth"
-export const { GET, POST } = handlers
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+})
+
+export { handler as GET, handler as POST }
